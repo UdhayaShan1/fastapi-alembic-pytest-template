9598e5f6862dd6a20d9f00463e4eb255e72c9e46
DESKTOP-S09N3DI\Udhaya <udhaya.shan1@gmail.com>
2025-11-09 20:24:06 +0800

    Add minor refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 import pytest
 from fastapi.testclient import TestClient
-from testcontainers.postgres import PostgresContainer  # type: ignore
+from testcontainers.postgres import PostgresContainer 
 from app.database import Base, get_db
 from sqlalchemy.orm import sessionmaker, Session
 
@@ -18,7 +18,7 @@ def postgres_container():
 
 @pytest.fixture(scope='function')
 def db_session(postgres_container: PostgresContainer):
-    conn_url: str = postgres_container.get_connection_url()  # type: ignore
+    conn_url: str = postgres_container.get_connection_url() 
     assert isinstance(conn_url, str)
     engine = create_engine(conn_url)
 
@@ -51,35 +51,3 @@ def client(db_session: Session):
     app.dependency_overrides.clear()
 
 
-@pytest.fixture(scope='function')
-def sample_movie_data(db_session: Session):
-    movies: list[Movie] = [
-        Movie(
-            name="Inception",
-            release_date=date(2010, 7, 16),
-            rating=8.8
-        ),
-        Movie(
-            name="The Dark Knight",
-            release_date=date(2008, 7, 18),
-            rating=9.0
-        ),
-        Movie(
-            name="Interstellar",
-            release_date=date(2014, 11, 7),
-            rating=8.6
-        ),
-        Movie(
-            name="The Matrix",
-            release_date=date(1999, 3, 31),
-            rating=8.7
-        ),
-        Movie(
-            name="Pulp Fiction",
-            release_date=date(1994, 10, 14),
-            rating=8.9
-        ),
-    ]
-
-    db_session.add_all(movies)
-    db_session.commit()
--- tests/test_movies.py
+++ tests/test_movies.py
@@ -1,6 +1,41 @@
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 from app.models import Movie
+import pytest
+from datetime import date
+
+@pytest.fixture(scope='function')
+def sample_movie_data(db_session: Session):
+    movies: list[Movie] = [
+        Movie(
+            name="Inception",
+            release_date=date(2010, 7, 16),
+            rating=8.8
+        ),
+        Movie(
+            name="The Dark Knight",
+            release_date=date(2008, 7, 18),
+            rating=9.0
+        ),
+        Movie(
+            name="Interstellar",
+            release_date=date(2014, 11, 7),
+            rating=8.6
+        ),
+        Movie(
+            name="The Matrix",
+            release_date=date(1999, 3, 31),
+            rating=8.7
+        ),
+        Movie(
+            name="Pulp Fiction",
+            release_date=date(1994, 10, 14),
+            rating=8.9
+        ),
+    ]
+
+    db_session.add_all(movies)
+    db_session.commit()
 
 
 def test_get_all_movies(client: TestClient, sample_movie_data):
